a50136ebbccc885502c183cb10880f92465d62bc
longdo0102 <125441005+longdo0102@users.noreply.github.com>
2023-02-21 15:41:36 +0700

    Update Cau5.dart

--- Cau5.dart
+++ Cau5.dart
@@ -3,7 +3,7 @@
 void main(List<String> args) {
   int? n = int.parse(stdin.readLineSync()!);
   int sum = 0;
-  for(int i = 0 ; i < n ; i++){
+  for(int i = 0 ; i <= n ; i++){
     sum = sum + i;
   }
   print("Tong cac so tu 0 den $n  = $sum");
